c6fdcdfebf54e49c39c3a2b5f2a618ecdf8e63b9
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-03-27 09:21:21 +0200

    latest version since 27 march 2015

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-	$("body").delay(1000).animate({opacity: 1}, 100);
-
-	$(".btn-about").click(function(){
-		$(".aboutpage").css("display", "flex");
-		$(".btn-about").css("display", "none");
-	})
-	$(".btn-close").click(function(){
-		$(".aboutpage").css("display", "none");
-		$(".btn-about").css("display", "block");
-	})
+	$("body").delay(1000).animate({opacity: 1}, 800);
 })
